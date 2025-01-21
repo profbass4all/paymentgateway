38d96504c8418766a20a260469c849c33fbf6e1a
Abass Ibrahim <profbass4all@gmail.com>
2025-01-21 08:06:10 +0100

    added vercel config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require("express");
 const bodyParser = require('body-parser')
-const PaymentRouter = require('../routes/payment.routes.js')
+const PaymentRouter = require('./routes/payment.routes.js')
 const app = express();
 const port = process.env.PORT || 3000;
 
